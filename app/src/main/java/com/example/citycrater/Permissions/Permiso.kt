cf677b2d97b391aaf70176c1d034afbf726940cf
BoredDany <martinezq.d@javeriana.edu.co>
2024-03-05 00:49:29 -0500

    camera

---
+++ app/src/main/java/com/example/citycrater/Permissions/Permiso.kt
@@ -0,0 +1,10 @@
+package com.example.citycrater.Permissions
+
+class Permiso {
+    companion object {
+        const val MY_PERMISSION_REQUEST_CAMERA = 1
+        const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+
+}
